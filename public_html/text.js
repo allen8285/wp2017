c6144d50aa50a26b7fd8bbe8b136c1dba422aee2
Allen <allen8285@gmail.com>
2017-10-27 19:13:32 +0800

    new

--- public_html/text.js
+++ public_html/text.js
@@ -1,22 +1,24 @@
-$('#ajax-form button[type=submit]').click((event) => {                          
+$('#ajax-form button[type=submit]').click((event) => {
   event.preventDefault()
     // no need if button type is not 'submit',
       //   but make it work without ajax is a good practice
 
-     $.get('try.php', {
-                        reply: $('#ajax-form input[name=reply]').val(),
+        $.get('try.php', {
+            reply: $('#ajax-form input[name=reply]').val(),
+                
+                }, (data) => {
+                    $('#ajax-output').html(data)
+                      
+                        })
+                        $.get('try.php', {                                                            
+                            
+                                    reply1: $('#ajax-form input[name=reply1]').val(), 
+                                            }, (data) => {
+                                                        
+                                                                        $('#ajax-output1').html(data)
+                                                                                          })
 
-                      }, (data) => {
-                                    $('#ajax-output').html(data)
 
-                                    }) 
-     $.get('try.php', {
-                       reply1: $('#ajax-form input[name=reply1]').val(),
-
-                      }, (data) => {
-                                    $('#ajax-output1').html(data)
-                                    })
-
-
-              // setTimeout(() => $('#ajax-output').html('loading'), 500)
-              // try to modify the timeout
+                                                                                           // setTimeout(() => $('#ajax-output').html('loading'), 500)
+                                                                                             // try to modify the timeout
+                                                                                             })
