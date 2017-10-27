1f9ff153bd6ffe378feffbd615c9a96e84c43251
Allen <allen8285@gmail.com>
2017-10-27 18:34:55 +0800

    js

---
+++ public_html/text.js
@@ -0,0 +1,22 @@
+$('#ajax-form button[type=submit]').click((event) => {                          
+  event.preventDefault()
+    // no need if button type is not 'submit',
+      //   but make it work without ajax is a good practice
+
+     $.get('try.php', {
+                        reply: $('#ajax-form input[name=reply]').val(),
+
+                      }, (data) => {
+                                    $('#ajax-output').html(data)
+
+                                    }) 
+     $.get('try.php', {
+                       reply1: $('#ajax-form input[name=reply1]').val(),
+
+                      }, (data) => {
+                                    $('#ajax-output1').html(data)
+                                    })
+
+
+              // setTimeout(() => $('#ajax-output').html('loading'), 500)
+              // try to modify the timeout
